33e89353ee73c0fa6819dce1c2e0b42cf2e99caf
Gabriel Campos <gabrielcampoos@hotmail.com>
2023-04-13 20:07:20 -0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -9,15 +8,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 		<App />
 	</React.StrictMode>,
 );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
 );
->>>>>>> b246020e28481336dd671349a786b063d47b05d1
